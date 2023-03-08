f098d0e94ff1e432d217ed1c8e3b54eea04ad07a
gerardoezequiel <geraezemc@gmail.com>
2023-03-08 16:36:56 +0000

    added map with basic style

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZWZhY3VuZG9hcmdhbmEiLCJhIjoiY2p3em8wNzkzMHV0eDN6cG9xMDkyY3MweCJ9.BFwFTr19FLGdPHqxA8qkiQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/geraezemc/ckhif49jm2l3x19ot4hmqsvz5",
+  attributionControl: true,
+  center: [-0.133583, 51.524776],
+  zoom: 14,
+});
