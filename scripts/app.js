ae2af58c4916182523b6b6e026b9e3b348b59176
gerardoezequiel <geraezemc@gmail.com>
2023-03-08 17:18:22 +0000

    added marker to UCL to start performing API queries with Isochrone API and POI's API

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,13 @@
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZWZhY3VuZG9hcmdhbmEiLCJhIjoiY2p3em8wNzkzMHV0eDN6cG9xMDkyY3MweCJ9.BFwFTr19FLGdPHqxA8qkiQ";
+let coordinatesUCL = [-0.133583, 51.524776]; // UCL coordinates to start the map and the isochrone
 
 // Create a map object with UCL coordinates as the center
 const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/geraezemc/ckhif49jm2l3x19ot4hmqsvz5",
-  attributionControl: true,
-  center: [-0.133583, 51.524776], // UCL coordinates
+  container: "map", // container ID
+  style: "mapbox://styles/geraezemc/ckhif49jm2l3x19ot4hmqsvz5", // style URL style created by Gerardo Ezequiel with love
+  attributionControl: true, // show attribution
+  center: coordinatesUCL, // UCL coordinates
   zoom: 14,
 });
 
@@ -24,7 +25,7 @@ map.addControl(
   })
 );
 
-// Add scale control to the map
+// Add scale bar control to the map
 map.addControl(
   new mapboxgl.ScaleControl({
     maxWidth: 100,
@@ -32,3 +33,11 @@ map.addControl(
   }),
   "bottom-right"
 );
+
+// Add a marker to the map
+const marker = new mapboxgl.Marker({
+  color: "#000000",
+  draggable: true,
+})
+  .setLngLat(coordinatesUCL)
+  .addTo(map);
