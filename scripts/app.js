b9226e354e299a98dc7979f030134286a781cf7c
gerardoezequiel <geraezemc@gmail.com>
2023-03-08 18:35:28 +0000

    added asynchronous function

--- scripts/app.js
+++ scripts/app.js
@@ -2,46 +2,6 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiZWZhY3VuZG9hcmdhbmEiLCJhIjoiY2p3em8wNzkzMHV0eDN6cG9xMDkyY3MweCJ9.BFwFTr19FLGdPHqxA8qkiQ";
 let coordinatesUCL = [-0.133583, 51.524776]; // UCL coordinates to start the map and the isochrone
 
-// Create a map object with UCL coordinates as the center
-const map = new mapboxgl.Map({
-  container: "map", // container ID
-  style: "mapbox://styles/geraezemc/ckhif49jm2l3x19ot4hmqsvz5", // style URL style created by Gerardo Ezequiel with love
-  attributionControl: true, // show attribution
-  center: coordinatesUCL, // UCL coordinates
-  zoom: 14,
-});
-
-// Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
-map.addControl(new mapboxgl.FullscreenControl());
-
-// Add geolocate control to the map
-map.addControl(
-  new mapboxgl.GeolocateControl({
-    positionOptions: {
-      enableHighAccuracy: true,
-    },
-    trackUserLocation: true,
-  })
-);
-
-// Add scale bar control to the map
-map.addControl(
-  new mapboxgl.ScaleControl({
-    maxWidth: 100,
-    unit: "metric",
-  }),
-  "bottom-right"
-);
-
-// Add a marker to the map
-const marker = new mapboxgl.Marker({
-  color: "#000000",
-  draggable: true,
-})
-  .setLngLat(coordinatesUCL)
-  .addTo(map);
-
 // We want the coordinates of the user
 // The function returns a promise, because we want to resolve the coordinates
 // We call the navigator.geolocation.getCurrentPosition() method
@@ -57,3 +17,47 @@ const getLocation = () => {
     );
   });
 };
+
+map.on("load", async () => {
+  // get coordinates of the user
+  const [longitude, latitude] = await getLocation();
+  // Create a map object with UCL coordinates as the center
+  const map = new mapboxgl.Map({
+    container: "map", // container ID
+    style: "mapbox://styles/geraezemc/ckhif49jm2l3x19ot4hmqsvz5", // style  URL style created by Gerardo Ezequiel with love
+    attributionControl: true, // show attribution
+    center: coordinatesUCL, // UCL coordinates
+    zoom: 14,
+  });
+
+  // Add zoom and rotation controls to the map.
+  map.addControl(new mapboxgl.NavigationControl());
+  map.addControl(new mapboxgl.FullscreenControl());
+
+  // Add geolocate control to the map
+  map.addControl(
+    new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+    })
+  );
+
+  // Add scale bar control to the map
+  map.addControl(
+    new mapboxgl.ScaleControl({
+      maxWidth: 100,
+      unit: "metric",
+    }),
+    "bottom-right"
+  );
+
+  // Add a marker to the map
+  const marker = new mapboxgl.Marker({
+    color: "#000000",
+    draggable: true,
+  })
+    .setLngLat(coordinatesUCL)
+    .addTo(map);
+});
