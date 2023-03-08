ed2ae099deb304bc6018579fab361f845710c8ee
gerardoezequiel <geraezemc@gmail.com>
2023-03-08 19:50:59 +0000

    adding isochrone layer and directions

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
+import { addIsoChrone } from "./isochrone.js";
+
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZWZhY3VuZG9hcmdhbmEiLCJhIjoiY2p3em8wNzkzMHV0eDN6cG9xMDkyY3MweCJ9.BFwFTr19FLGdPHqxA8qkiQ";
-let coordinatesUCL = [-0.133583, 51.524776]; // UCL coordinates to start the map and the isochrone
 
 // We want the coordinates of the user
 // The function returns a promise, because we want to resolve the coordinates
@@ -17,6 +18,7 @@ const getLocation = () => {
     );
   });
 };
+
 // We add a DOMContentLoaded event listener to our window object. This event listener will be called when the DOM content has been loaded.
 window.addEventListener("DOMContentLoaded", async () => {
   // get coordinates of the user
@@ -26,7 +28,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     container: "map", // container ID
     style: "mapbox://styles/geraezemc/ckhif49jm2l3x19ot4hmqsvz5", // style  URL style created by Gerardo Ezequiel with love
     attributionControl: true, // show attribution
-    center: coordinatesUCL, // UCL coordinates
+    center: [longitude, latitude], // UCL coordinates
     zoom: 14,
   });
 
@@ -53,11 +55,73 @@ window.addEventListener("DOMContentLoaded", async () => {
     "bottom-right"
   );
 
+  map.addControl(directions, "bottom-left");
+
   // Add a marker to the map
   const marker = new mapboxgl.Marker({
-    color: "#000000",
-    draggable: true,
+    color: "#f3a64f",
+    draggable: false,
   })
-    .setLngLat(coordinatesUCL)
+    .setLngLat([longitude, latitude])
     .addTo(map);
-});
+
+  //Isochrone API Mapbox
+
+  const params = document.getElementById("params");
+
+  // Create variables to use in getIso()
+  const urlBase = "https://api.mapbox.com/isochrone/v1/mapbox/";
+  let profile = "walking";
+  let minutes = 15;
+
+  //Create a function that sets up the Isochrone API query then makes a call
+  const getIso = async () => {
+    const query = `${urlBase}${profile}/${longitude},${latitude}?contours_minutes=${minutes}&polygons=true&access_token=${mapboxgl.accessToken}`;
+    const response = await fetch(query);
+    const data = await response.json();
+    map.getSource("iso").setData(data);
+  };
+  //If the user click in the buttom the value (html) it's passed to the query
+  const onChangeParams = async (event) => {
+    if (event.target.name === "profile") {
+      profile = event.target.value;
+      await getIso();
+    } else if (event.target.name === "duration") {
+      minutes = event.target.value;
+      await getIso();
+    }
+  };
+
+  // When a user changes the value of profile or duration by clicking a button, change the parameter's value and make the API query again
+
+  params.addEventListener("change", onChangeParams);
+
+  map.on("load", async () => {
+    await addIsoChrone({ map, marker, getIso, longitude, latitude });
+    directions.setOrigin([longitude, latitude]);
+  });
+
+  //Adding the direction controller with IP direction as origin
+  const directions = new MapboxDirections({
+    unit: "metric",
+    profile: "mapbox/walking",
+    setOrigin: [longitude, latitude],
+    alternatives: true,
+    accessToken: mapboxgl.accessToken,
+  });
+  directions.on("profile", async (event) => {
+    const transportMethod = event.profile;
+    const directionMethod = transportMethod.split("/").pop();
+    profile = directionMethod;
+    await getIso();
+  });
+  map.addControl(directions, "bottom-left");
+
+  //Removing the driving and driving traffic buttom
+  document
+    .querySelector('label[for="mapbox-directions-profile-driving-traffic"]')
+    .remove();
+  document
+    .querySelector('label[for="mapbox-directions-profile-driving"]')
+    .remove();
+}); // end of window onload
---
+++ scripts/isochrone.js
@@ -0,0 +1,31 @@
+export const addIsoChrone = async ({
+  map,
+  getIso,
+  marker,
+  longitude,
+  latitude,
+}) => {
+  map.addSource("iso", {
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: [],
+    },
+  });
+  await getIso();
+
+  map.addLayer(
+    {
+      id: "Isochrone",
+      type: "fill",
+      source: "iso",
+      // layout: { visibility: 'none' },
+      paint: {
+        "fill-color": "#5a3fc0",
+        "fill-opacity": 0.3,
+      },
+    },
+    "poi-label"
+  );
+  marker.setLngLat({ lon: longitude, lat: latitude }).addTo(map);
+};
